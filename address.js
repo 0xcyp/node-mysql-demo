54bd60a349c22641686c88ce3932da5990796b74
Ziad Saab <ziad.saab@gmail.com>
2015-09-28 16:08:02 +0000

    Added addressbook example
    - `related-data.sql` contains 3 different ways of doing "joins" with mysql

--- address.js
+++ address.js
@@ -10,3 +10,29 @@ var connection = mysql.createConnection({
   password : '',
   database : 'addressbook'
 });
+
+connection.queryAsync('SELECT * FROM Account LIMIT 10').then(
+    function(accountResults) {
+        return accountResults[0]; // rows from mysql
+    }
+)
+.map(
+    function(accountResult) {
+        return connection.queryAsync("SELECT * FROM AddressBook WHERE accountId = " + accountResult.id).then(
+            function(abResults) {
+                accountResult.addressBooks = abResults[0];
+                return accountResult;
+            }
+        );
+    }
+)
+.then(
+    function(accountsWithAddressBooks) {
+        console.log(require('util').inspect(accountsWithAddressBooks, true, 10));
+    }
+)
+.finally(
+    function() {
+        connection.end();
+    }
+);
