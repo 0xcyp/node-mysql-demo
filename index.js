ce7b1e4f34c1a84126ba164643fe0fb78446105f
Ziad Saab <ziad.saab@gmail.com>
2015-09-28 15:17:50 +0000

    First pass at going thru mapped queries

--- index.js
+++ index.js
@@ -17,20 +17,40 @@ var connection = mysql.createConnection({
 //   console.log('The solution is: ', rows[0].solution);
 // });
 
-connection.queryAsync('SELECT 1 + 1 AS solution').then(
-    function(result) {
-        var rows = result[0];
-        console.log("The rows of the query are: ", rows);
-        
-        return connection.queryAsync('SHOW DATABASES');
-    }
-).then(
+connection.queryAsync('SHOW DATABASES').then(
     function(result) {
         var rows = result[0];
         console.log("The databases in this mysql instance are: ", rows);
+        return rows;
+    }
+).map(
+    function(row) {
+        return connection.queryAsync('SHOW TABLES FROM ' + row.Database)
+    }
+).then(
+    function(mappedRows) {
+        console.dir(mappedRows[0]); // each of the mappedRows is a `queryAsync` result. it's an array of [rows, fields]
+        // console.log("These are all the tables from each database:");
+        for (var i = 0; i < mappedRows.length; i++) {
+            var mappedRow = mappedRows[i];
+            console.log(mappedRow);
+        }
     }
 ).finally(
     function() {
         connection.end();
     }
-)
+);
+
+// Array mapping example
+// var myArray = [10, 15, 20];
+// var myMappedArray = myArray.map(function(item) {
+//     return item * 10;
+// });
+
+// console.log(myArray, myMappedArray);
+
+// This is NOT real code
+// var mappedRows = rows.map(function(row) {
+//     return connection.query("SHOW TABLES FROM " + row.Database);
+// });
